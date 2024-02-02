52004f8dd0c7df7f95fde7381778fd993231d8b5
Derek <dtan56@uwo.ca>
2024-02-02 12:37:04 -0500

    Commiting the changes to my local repo

---
+++ Lab3App/Collectable.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3App
+{
+    class Collectable : Displayable
+    {
+        // Field of CollectionBoard type
+        private CollectionBoard board;
+        private string description;
+
+        public CollectionBoard Board
+        {
+            get { return board; }
+            set { board = value; }
+        }
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+        public Collectable(string description)
+        {
+            Description = description;
+        }
+        public virtual void AddMe(List<Collectable> list)
+        {
+            list.Add(this);
+            Console.WriteLine($"{Description} Collected, Congrats!!!!");
+        }
+
+        public virtual void Display()
+        {
+            Console.WriteLine($"{Description} is displayed");
+        }
+    }
+}
---
+++ Lab3App/Treasure.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3App
+{
+    class Treasure : Collectable
+    {
+        private int score;
+        public int Score
+        {
+            get { return score; }
+            set { score = value; }
+        }
+        public Treasure(string description, int score) : base(description)
+        {
+            Score = score;
+
+        }
+        public virtual void UpdateTotalScore()
+        {
+            Board.TotalScore += Score;
+            Console.WriteLine($"Total Score is updated to: {Board.TotalScore}");
+
+        }
+        public override void AddMe(List<Collectable> list)
+        {
+            base.AddMe(list);
+            UpdateTotalScore();
+        }
+    }
+}
